34bf00692c87810eda9888f18e6f500cd67d5895
snrnapa <shino.satoru@gmail.com>
2024-11-19 22:47:16 +0900

    背景の写真を設定した

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ const Hero = () => (<div className="relative pt-16 pb-32 flex content-center ite
   }}>
   <div className="absolute top-0 w-full h-full bg-center bg-cover"
     style={{
-      backgroundImage: "url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')"
+      backgroundImage: "url('/img/back-image.webp')"
     }}>
     <span id="blackOverlay" className="w-full h-full absolute opacity-75 bg-black"></span>
   </div>
